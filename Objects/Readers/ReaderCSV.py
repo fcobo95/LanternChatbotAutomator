6fbfbf913190ba46ff1a0413ade8dddbce611dd7
Krakenaitor <fcobo95@gmail.com>
2017-12-04 09:15:16 -0600

    Creating a more robust and organized directory tree structure. Added CSV Files, API Key and the possible Excel File to use for the final Chatbot body of knowledge inputs.

--- Objects/Readers/ReaderCSV.py
+++ Objects/Readers/ReaderCSV.py
@@ -7,5 +7,5 @@ class ReaderCSV:
     """
 
     def __init__(self):
-        with open('../Data Files/data.csv') as csv_data:
+        with open('../Data Files/MainCSV.csv') as csv_data:
             self.final_csv_data = pandas.read_csv(csv_data, delimiter=',', skipinitialspace=True)
